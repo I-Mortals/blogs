13f18b30d559ec27221da08c2cd022ff4faf8c87
潘洪成 <not_trace@163.com>
2021-07-22 00:46:38 +0800

    测试1

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,7 @@
 # 确保脚本抛出遇到的错误
 set -e
 
+echo 开始编译
 # 生成静态文件
 npm run build
 
@@ -14,11 +15,11 @@ cd docs/.vuepress/dist
 
  if [ -z "$GITHUB" ]; then
    msg='deploy'
-   echo 秘钥
+   echo 秘钥Github
    githubUrl=git@github.com:not-trace/blogs.git
  else
    msg='来自github actions的自动部署'
-   echo 个人令牌
+   echo 个人令牌Github
    githubUrl=https://not-trace:${GITHUB}@github.com/not-trace/blogs.git
    git config --global user.name "Mortal红尘"
    git config --global user.email "not_trace@163.com"
@@ -30,10 +31,10 @@ cd docs/.vuepress/dist
 
 # deploy to coding
 if [ -z "$CODING" ]; then  # -z 字符串 长度为0则为true；$CODING来自于github仓库`Settings/Secrets`设置的私密环境变量
-  echo 秘钥
+  echo 秘钥Coding
   codingUrl=git@e.coding.net:zszl/blog/blog.git
 else
-  echo 个人令牌
+  echo 个人令牌Coding
   codingUrl=https://WxQGVvHQav:${CODING}@e.coding.net:zszl/blog/blog.git
 fi
 git push -f $codingUrl master # 推送到coding
