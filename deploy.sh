5b5f41476af7b014af65a50e281e3dd225465685
潘洪成 <not_trace@163.com>
2021-07-21 23:21:44 +0800

    部署

--- deploy.sh
+++ deploy.sh
@@ -24,12 +24,11 @@ cd docs/.vuepress/dist
  git commit -m "${msg}"
  git push -f $githubUrl master:pages # 推送到github pages分支
 
-# git@e.coding.net:zszl/blog/blog.git
 # deploy to coding
-if [ -z "$CODING" ]; then  # -z 字符串 长度为0则为true；$CODING_TOKEN来自于github仓库`Settings/Secrets`设置的私密环境变量
-  codingUrl=git@git.dev.tencent.com:zszl/blog/blog.git
+if [ -z "$CODING" ]; then  # -z 字符串 长度为0则为true；$CODING来自于github仓库`Settings/Secrets`设置的私密环境变量
+  codingUrl=git@e.coding.net:zszl/blog/blog.git
 else
-  codingUrl=https://xugaoyi:${CODING}@git.dev.tencent.com/zszl/blog/blog.git
+  codingUrl=https://WxQGVvHQav:${CODING}@e.coding.net:zszl/blog/blog.git
 fi
 git add -A
 git commit -m "${msg}"
