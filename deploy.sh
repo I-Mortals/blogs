a0429ae7f54c0fac704dfdec74a2c4544b821ad5
潘洪成 <not_trace@163.com>
2021-07-22 00:19:30 +0800

    环境1

--- deploy.sh
+++ deploy.sh
@@ -14,9 +14,11 @@ cd docs/.vuepress/dist
 
  if [ -z "$GITHUB" ]; then
    msg='deploy'
+   echo 秘钥
    githubUrl=git@github.com:not-trace/blogs.git
  else
    msg='来自github actions的自动部署'
+   echo 个人令牌
    githubUrl=https://not-trace:${GITHUB}@github.com/not-trace/blogs.git
    git config --global user.name "Mortal红尘"
    git config --global user.email "not_trace@163.com"
@@ -24,15 +26,16 @@ cd docs/.vuepress/dist
  git init
  git add -A
  git commit -m "${msg}"
- git push -f $githubUrl main:pages # 推送到github pages分支
+ git push -f $githubUrl master:pages # 推送到github pages分支
 
 # deploy to coding
 if [ -z "$CODING" ]; then  # -z 字符串 长度为0则为true；$CODING来自于github仓库`Settings/Secrets`设置的私密环境变量
+  echo 秘钥
   codingUrl=git@e.coding.net:zszl/blog/blog.git
 else
+  echo 个人令牌
   codingUrl=https://WxQGVvHQav:${CODING}@e.coding.net:zszl/blog/blog.git
 fi
-
 git push -f $codingUrl master # 推送到coding
 cd -
 rm -rf docs/.vuepress/dist
