e35d82080c86561010a2ce66672621256968d8cf
潘洪成 <not_trace@163.com>
2021-08-01 00:21:50 +0800

    add .

--- docs/.vuepress/config/plugins.js
+++ docs/.vuepress/config/plugins.js
@@ -1,7 +1,7 @@
 // 插件配置
 module.exports = [
   // 本地插件
-  [require('../plugins/love-me/index.js'), { // 鼠标点击爱心特效
+  [require('../plugins/love-me/index.js'), { // 鼠标点击爱心特效.
     // color: '#11a8cd', // 爱心颜色，默认随机色
     excludeClassName: 'theme-vdoing-content' // 要排除元素的class, 默认空''
   }],
