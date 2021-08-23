1dc6ac2f3f50063fe1a6f7fcdbe74c11e3489813
not-trace <57317180+not-trace@users.noreply.github.com>
2021-08-23 13:35:53 +0800

    Update nav.js

--- docs/.vuepress/config/nav.js
+++ docs/.vuepress/config/nav.js
@@ -9,7 +9,7 @@ module.exports = [
       {
         text: '前端学习笔记',
         items: [
-          { text: '前端', link: '/pages/qldidh56/' },
+          { text: '前端', link: '/note/tsbiji01/' },
         ],
       },
     ],
