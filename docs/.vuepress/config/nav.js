aef3e25764eade4129a6a42817dab768ce6cfd1d
潘洪成 <not_trace@163.com>
2021-09-07 22:48:37 +0800

    博客更新

--- docs/.vuepress/config/nav.js
+++ docs/.vuepress/config/nav.js
@@ -7,9 +7,10 @@ module.exports = [
     items: [
       // 说明：以下所有link的值只是在相应md文件定义的永久链接（不是什么特殊生成的编码）。另外，注意结尾是有斜杠的
       {
-        text: '前端学习笔记',
+        text: '学习笔记',
         items: [
           { text: '前端', link: '/note/tsbiji01/' },
+          { text: '后端', link: '/note/gobiji96/' },
         ],
       },
     ],
